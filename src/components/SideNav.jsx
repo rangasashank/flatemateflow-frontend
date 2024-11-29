2db19af17a4643fe245a4f2d1e51f65e190b99f4
Ranga Sashank Gurram <rangasashakgurram@uvic.ca>
2024-11-29 02:37:16 -0800

    nav bar animation issue

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -25,7 +25,7 @@ const SideNav = () => {
 
     // Cleanup listener on unmount
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [isOpen]);
 
   return (
     <>
